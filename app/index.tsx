2de8d98c00de10cd74c346e542cf432bc927d23a
Filippo Petito <filippo.petito67@gmail.com>
2025-06-28 20:04:18 +0200

    Dummy commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,7 @@
 import { Text, View } from "react-native";
 import {Link} from "expo-router";
-import {StatusBar} from "expo-status-bar";
-import {Theme} from "@/services/Settings/settings";
 
 export default function Index() {
-
-
   return (
     <View
       style={{
