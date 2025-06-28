a1a0d6568065dc3b832ab279200bf06c47995b60
Filippo Petito <filippo.petito67@gmail.com>
2025-06-28 20:00:32 +0200

    Cleaning code

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View } from "react-native";
+import {Link} from "expo-router";
+import {StatusBar} from "expo-status-bar";
+import {Theme} from "@/services/Settings/settings";
+
+export default function Index() {
+
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Link href="/details">View details</Link>
+    </View>
+  );
+}
